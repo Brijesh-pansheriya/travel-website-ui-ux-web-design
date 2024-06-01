60eeb230676eff81fbaea87f9671425b3e4645d5
Brijesh-pansheriya <b.pansheriya2003@gmail.com>
2024-06-02 01:07:02 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
+
+export const metadata = {
+  title: "Travel",
+  description: "Travel UI/UX Design",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+import Camp from "@/Components/Camp";
+import Features from "@/src/Components/Features";
+import GetApp from "@/src/Components/GetApp";
+import Guide from "@/src/Components/Guide";
+import Hero from "@/src/Components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
